8d52511554b48648beb0fb8e3b16515a243fd4a3
timetobehappy <jibiu@outlook.com>
2017-02-22 13:39:45 -0600

    new code to test bot list from a file. This needs be removed or moved to a new repo later

--- server.js
+++ server.js
@@ -3,8 +3,20 @@ const path = require('path');
 const moment = require('moment');
 const hbs = require('hbs');
 const fs = require('fs')
+const request = require('request');
+
 
 const port = process.env.PORT || 3000;
+
+var options = {
+    //url: 'http://m.kmart.com/toys-games-dolls-accessories-baby-dolls/b-20722',
+    headers: {
+        'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0.1; Nexus 5X Build/MMB29P) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.96 Mobile Safari/537.36 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'
+            //'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1 (KHTML, like Gecko) CriOS/58.0.3019.0 Mobile/13B143 Safari/601.1.46'
+    }
+};
+
+var lineReader;
 
 var app = express();
 //var currentYear = moment().format('YYYY');
@@ -15,6 +27,47 @@ hbs.registerHelper('getCurrentYear', () => {
 hbs.registerHelper('toUpperCaseHelper', (text) => {
     return text.toUpperCase();
 })
+
+hbs.registerHelper('botList', () => {
+
+    lineReader = require('readline').createInterface({
+        input: require('fs').createReadStream('mobiletestgroup_small.txt')
+    });
+
+
+
+    var out = "<ul>";
+
+    lineReader.on('line', function(line) {
+        options.uri = line;
+        console.log(options.uri);
+        request.head(options, function(error, response, body) {
+            if (!error && response.statusCode == 200) {
+
+                //console.log(typeof(JSON.stringify(response.headers).indexOf('phantomjs-workerid')));
+                if (JSON.stringify(response.headers).indexOf('phantomjs-workerid') == -1) {
+                    out = out + "<li>" + `${line} is going to Prod` + "</li>";
+                    //console.log(`${options.url} is going to Prod`);
+                } else {
+                    //console.log(`${options.url} is going to Bot`);
+                    out = out + "<li>" + `${line} is going to Bot` + "</li>";
+                }
+
+            } else {
+                console.log(error);
+                //console.log(`${line} gave a http response of ${response.statusCode}`);
+                //out = out + "<li>" + `${line} gave a http response of ${response.statusCode}` + "</li>";
+            }
+        });
+
+        out = out + "<li>" + "line" + "</li>";
+    });
+
+
+
+    return out + "</ul>";
+
+});
 hbs.registerPartials(path.join(__dirname, '/views/partials'));
 app.set('view engine', 'hbs');
 
@@ -22,11 +75,10 @@ app.use((req, res, next) => {
     var now = new Date().toString();
     var log = `${now}: ${req.method} ${req.url}`;
     console.log(log);
-    fs.appendFile('server.log', log + '\n', (err)=>{
-      if(err)
-      {
-        console.log('Unable to append to server.log.')
-      }
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('Unable to append to server.log.')
+        }
     })
 
     next();

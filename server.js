77add2452e02f105c3be9a2dccfa82b2069d2811
timetobehappy <jibiu@outlook.com>
2017-02-22 16:31:18 -0600

    added bot check temp case

--- server.js
+++ server.js
@@ -1,22 +1,62 @@
+const request = require('request');
 const express = require('express');
 const path = require('path');
 const moment = require('moment');
 const hbs = require('hbs');
 const fs = require('fs')
-const request = require('request');
+
 
 
 const port = process.env.PORT || 3000;
+
+
+//var lineReader;
+var out = "<ul id=myList>";
+
 
 var options = {
     //url: 'http://m.kmart.com/toys-games-dolls-accessories-baby-dolls/b-20722',
     headers: {
         'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0.1; Nexus 5X Build/MMB29P) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.96 Mobile Safari/537.36 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'
-            //'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1 (KHTML, like Gecko) CriOS/58.0.3019.0 Mobile/13B143 Safari/601.1.46'
+        //'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1 (KHTML, like Gecko) CriOS/58.0.3019.0 Mobile/13B143 Safari/601.1.46'
     }
 };
 
-var lineReader;
+var getResponseForUrls = function() {
+
+  var lineReader = require('readline').createInterface({
+      input: require('fs').createReadStream('mobiletestgroup.txt')
+  });
+
+  console.log('Inside getResponseForUrls');
+
+    lineReader.on('line', function(line) {
+        console.log('Line from file:', line);
+
+
+        options.url = line;
+        //options.headers.'User-Agent'="Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1 (KHTML, like Gecko) CriOS/58.0.3019.0 Mobile/13B143 Safari/601.1.46";
+        request.head(options, function(error, response, body) {
+            if (!error && response.statusCode == 200) {
+
+                //console.log(typeof(JSON.stringify(response.headers).indexOf('phantomjs-workerid')));
+                if (JSON.stringify(response.headers).indexOf('phantomjs-workerid') == -1) {
+                    console.log(`${line} is going to Production`);
+                    out = out + "<li>" + `${line} is going to Prod` + "</li>";
+                } else {
+                    console.log(`${line} is going to Bot`);
+                    out = out + "<li>" + `${line} is going to Bot` + "</li>";
+                }
+
+            } else {
+                console.log(`${error} for ${line}`);
+            }
+        });
+    });
+
+
+
+}
 
 var app = express();
 //var currentYear = moment().format('YYYY');
@@ -30,42 +70,19 @@ hbs.registerHelper('toUpperCaseHelper', (text) => {
 
 hbs.registerHelper('botList', () => {
 
-    lineReader = require('readline').createInterface({
-        input: require('fs').createReadStream('mobiletestgroup_small.txt')
-    });
+    var temp;
+
+    console.log('Inside botList');
+
+    getResponseForUrls();
 
 
 
-    var out = "<ul>";
 
-    lineReader.on('line', function(line) {
-        options.uri = line;
-        console.log(options.uri);
-        request.head(options, function(error, response, body) {
-            if (!error && response.statusCode == 200) {
+    temp=out;
+    out = "<ul id=myList>";
 
-                //console.log(typeof(JSON.stringify(response.headers).indexOf('phantomjs-workerid')));
-                if (JSON.stringify(response.headers).indexOf('phantomjs-workerid') == -1) {
-                    out = out + "<li>" + `${line} is going to Prod` + "</li>";
-                    //console.log(`${options.url} is going to Prod`);
-                } else {
-                    //console.log(`${options.url} is going to Bot`);
-                    out = out + "<li>" + `${line} is going to Bot` + "</li>";
-                }
-
-            } else {
-                console.log(error);
-                //console.log(`${line} gave a http response of ${response.statusCode}`);
-                //out = out + "<li>" + `${line} gave a http response of ${response.statusCode}` + "</li>";
-            }
-        });
-
-        out = out + "<li>" + "line" + "</li>";
-    });
-
-
-
-    return out + "</ul>";
+    return temp + "</ul>"+"<br>";
 
 });
 hbs.registerPartials(path.join(__dirname, '/views/partials'));

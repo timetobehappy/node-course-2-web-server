2f4c047039eec06a5471104f432c46ef3c064a98
timetobehappy <jibiu@outlook.com>
2017-02-23 15:03:20 -0600

    Added Akamai Pragma for Desktop too

--- server.js
+++ server.js
@@ -17,8 +17,9 @@ var out = "<ul id=myList>";
 var options = {
     //url: 'http://m.kmart.com/toys-games-dolls-accessories-baby-dolls/b-20722',
     headers: {
-        'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0.1; Nexus 5X Build/MMB29P) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.96 Mobile Safari/537.36 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'
-        //'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1 (KHTML, like Gecko) CriOS/58.0.3019.0 Mobile/13B143 Safari/601.1.46'
+        'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0.1; Nexus 5X Build/MMB29P) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.96 Mobile Safari/537.36 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)',
+        //'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1 (KHTML, like Gecko) CriOS/58.0.3019.0 Mobile/13B143 Safari/601.1.46',
+        'Pragma': 'akamai-x-get-client-ip, akamai-x-cache-on, akamai-x-cache-remote-on, akamai-x-check-cacheable, akamai-x-get-cache-key, akamai-x-get-extracted-values, akamai-x-get-nonces, akamai-x-get-ssl-client-session-id, akamai-x-get-true-cache-key, akamai-x-serial-no, akamai-x-feo-trace, akamai-x-get-request-id'
     }
 };
 
@@ -38,13 +39,16 @@ var getResponseForUrls = function() {
         //options.headers.'User-Agent'="Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1 (KHTML, like Gecko) CriOS/58.0.3019.0 Mobile/13B143 Safari/601.1.46";
         request.head(options, function(error, response, body) {
             if (!error && response.statusCode == 200) {
-
-                //console.log(typeof(JSON.stringify(response.headers).indexOf('phantomjs-workerid')));
-                if (JSON.stringify(response.headers).indexOf('phantomjs-workerid') == -1) {
-                    //console.log(`${line} is going to Production`);
+                console.log('\n');
+                console.log((JSON.stringify(response.headers)));
+                console.log('\n');
+                console.log('\n');
+                //if (JSON.stringify(response.headers).indexOf('phantomjs-workerid') == -1) {
+                if (JSON.stringify(response.headers).indexOf("ORIGIN_INDICATOR=BOT") == -1) {
+                    console.log(`${line} is going to Production`);
                     //out = out + "<li>" + `${line} is going to Prod` + "</li>";
                     out = out + "<li>" + `.........` + "</li>";
-                } else {
+                } else if (JSON.stringify(response.headers).indexOf("ORIGIN_INDICATOR=PROD") == -1) {
                     console.log(`${line} is going to Bot`);
                     out = out + "<li>" + `${line} is going to Bot` + "</li>";
                 }
@@ -77,7 +81,8 @@ var getResponseForUrlsDesk = function() {
             if (!error && response.statusCode == 200) {
 
                 //console.log(typeof(JSON.stringify(response.headers).indexOf('phantomjs-workerid')));
-                if (JSON.stringify(response.headers).indexOf('phantomjs-workerid') == -1) {
+                if (JSON.stringify(response.headers).indexOf("ORIGIN_INDICATOR=BOT") == -1) {
+                //if (JSON.stringify(response.headers).indexOf('phantomjs-workerid') == -1) {
                     console.log(`${line} is going to Production`);
                     out = out + "<li>" + `${line} is going to Prod` + "</li>";
                 } else {

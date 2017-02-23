f9153b80b2e056986a0be87f08ce19965653a651
timetobehappy <jibiu@outlook.com>
2017-02-23 09:46:53 -0600

    Removed the prod logging

--- server.js
+++ server.js
@@ -17,8 +17,8 @@ var out = "<ul id=myList>";
 var options = {
     //url: 'http://m.kmart.com/toys-games-dolls-accessories-baby-dolls/b-20722',
     headers: {
-        'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0.1; Nexus 5X Build/MMB29P) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.96 Mobile Safari/537.36 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'
-        //'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1 (KHTML, like Gecko) CriOS/58.0.3019.0 Mobile/13B143 Safari/601.1.46'
+        //'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0.1; Nexus 5X Build/MMB29P) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.96 Mobile Safari/537.36 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'
+        'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1 (KHTML, like Gecko) CriOS/58.0.3019.0 Mobile/13B143 Safari/601.1.46'
     }
 };
 
@@ -41,7 +41,7 @@ var getResponseForUrls = function() {
 
                 //console.log(typeof(JSON.stringify(response.headers).indexOf('phantomjs-workerid')));
                 if (JSON.stringify(response.headers).indexOf('phantomjs-workerid') == -1) {
-                    console.log(`${line} is going to Production`);
+                    //console.log(`${line} is going to Production`);
                     out = out + "<li>" + `${line} is going to Prod` + "</li>";
                 } else {
                     console.log(`${line} is going to Bot`);
